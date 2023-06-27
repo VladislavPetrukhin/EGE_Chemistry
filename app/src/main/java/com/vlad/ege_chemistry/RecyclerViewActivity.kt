c010dcdbcaea55c60e15684a5e33124c3f82a295
Владислав <vlad.petrukhin03@mail.ru>
2023-06-27 14:29:41 +0300

    fixes TrialVariantsActivity, it works. need some design fixes

--- app/src/main/java/com/vlad/ege_chemistry/RecyclerViewActivity.kt
+++ app/src/main/java/com/vlad/ege_chemistry/RecyclerViewActivity.kt
@@ -2,9 +2,17 @@ package com.vlad.ege_chemistry
 
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.PopupWindow
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.GridLayoutManager
@@ -13,6 +21,7 @@ import com.vlad.ege_chemistry.databinding.ActivityRecyclerviewBinding
 class RecyclerViewActivity : AppCompatActivity() {
 
     private val recyclerViewItems: ArrayList<String> = ArrayList<String>()
+    lateinit var binding: ActivityRecyclerviewBinding
     private val TAG = "LogRecyclerViewActivity"
     private var userSelectedMode = ""
 
@@ -21,114 +30,205 @@ class RecyclerViewActivity : AppCompatActivity() {
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val binding = DataBindingUtil.setContentView<ActivityRecyclerviewBinding>(this, R.layout.activity_recyclerview)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_recyclerview)
         userSelectedMode = intent.getStringExtra("userSelectedMode").toString()
-
+        val sharedPref: SharedPreferences =
+            this.getSharedPreferences("MyPref", Context.MODE_PRIVATE)
         // Сохраняем значение userSelectedMode в SharedPreferences
-        val sharedPref = this.getSharedPreferences("MyPref", Context.MODE_PRIVATE)
         val editor = sharedPref.edit()
         editor.putString("userSelectedMode", userSelectedMode)
         editor.apply()
+
+        Log.d(TAG, userSelectedMode)
+        createRecyclerView()
+    }
+
+    private fun createRecyclerView() {
+        inflateRecyclerViewItems()
+
         var columnCount = 1
-        if(userSelectedMode == "selectTrialVariantExercise"){
+        if (userSelectedMode == "selectTrialVariantExercise") {
             columnCount = 4
         }
 
-        Log.d(TAG,userSelectedMode)
-        inflateRecyclerViewItems()
-
-        val adapter = RecyclerViewAdapter(recyclerViewItems,this)
-        val layoutManager = GridLayoutManager(this,columnCount)
+        val adapter = RecyclerViewAdapter(recyclerViewItems, this)
+        val layoutManager = GridLayoutManager(this, columnCount)
 
         binding.recyclerView.adapter = adapter
         binding.recyclerView.layoutManager = layoutManager
     }
 
-    private fun inflateRecyclerViewItems(){
-        Log.d(TAG,userSelectedMode)
-        when(userSelectedMode){
+    private fun inflateRecyclerViewItems() {
+        Log.d(TAG, userSelectedMode)
+        recyclerViewItems.clear()
+        when (userSelectedMode) {
             "teory" -> {
-               val count = resources.getInteger(R.integer.teory_count)
+                val count = resources.getInteger(R.integer.teory_count)
                 var textResourceId: Int
-                for (i in 1..count){
-                    textResourceId = resources.getIdentifier("teory$i","string",packageName)
+                for (i in 1..count) {
+                    textResourceId = resources.getIdentifier("teory$i", "string", packageName)
                     recyclerViewItems.add(resources.getString(textResourceId))
                 }
             }
+
             "test" -> {
                 val count = resources.getInteger(R.integer.test_count)
                 var textResourceId: Int
-                for (i in 1..count){
-                    textResourceId = resources.getIdentifier("test_name$i","string",packageName)
+                for (i in 1..count) {
+                    textResourceId = resources.getIdentifier("test_name$i", "string", packageName)
                     recyclerViewItems.add(resources.getString(textResourceId))
                 }
             }
+
             "trialVariants" -> {
                 val count = resources.getInteger(R.integer.trial_variants_count)
                 var textResourceId: Int
-                for (i in 1..count){
-                    textResourceId = resources.getIdentifier("prName$i","string",packageName)
+                for (i in 1..count) {
+                    textResourceId = resources.getIdentifier("prName$i", "string", packageName)
                     recyclerViewItems.add(resources.getString(textResourceId))
                 }
             }
+
             "selectTrialVariantExercise" -> {
-                for (i in 1..29){
+                for (i in 1..29) {
                     recyclerViewItems.add(i.toString())
                 }
                 recyclerViewItems.add("Проверка")
                 recyclerViewItems.add("Удалить ответы")
-        }else -> {
-            Log.e(TAG,"ERROR")
+            }
+
+            else -> {
+                Log.e(TAG, "ERROR")
             }
         }
     }
-    fun goToActivity(position: Int){
+
+    fun goToActivity(position: Int) {
         // Получаем значение userSelectedMode из SharedPreferences
-        val sharedPref = applicationContext.getSharedPreferences("MyPref", Context.MODE_PRIVATE)
+        val sharedPref: SharedPreferences =
+            this.getSharedPreferences("MyPref", Context.MODE_PRIVATE)
         val userSelectedMode = sharedPref.getString("userSelectedMode", "").toString()
         Log.d(TAG, userSelectedMode)
 
         var intent = Intent()
-        when(userSelectedMode){
-            "teory"->{
-                intent = Intent(this,TeoryActivity::class.java)
+        when (userSelectedMode) {
+            "teory" -> {
+                intent = Intent(this, TeoryActivity::class.java)
             }
-            "test"->{
-                intent = Intent(this,TestActivity::class.java)
+
+            "test" -> {
+                intent = Intent(this, TestActivity::class.java)
             }
-            "trialVariants"->{
-                intent = Intent(this,RecyclerViewActivity::class.java)
-                intent.putExtra("userSelectedMode","selectTrialVariantExercise")
+
+            "trialVariants" -> {
+                intent = Intent(this, RecyclerViewActivity::class.java)
+                intent.putExtra("userSelectedMode", "selectTrialVariantExercise")
                 val editor = sharedPref.edit()
-                editor.putString("prNumber",(position+1).toString())
+                editor.putString("prNumber", (position + 1).toString())
                 editor.apply()
+                clearAnswers()
             }
-            "selectTrialVariantExercise"->{
+
+            "selectTrialVariantExercise" -> {
                 when (position) {
                     29 -> {
-                        TrialVariantsActivity().checkAnswers()
+                        checkAnswers()
+                        return
                     }
+
                     30 -> {
-                        TrialVariantsActivity().clearAnswers()
+                        clearAnswers()
+                        return
                     }
+
                     else -> {
-                        intent = Intent(this,TrialVariantsActivity::class.java)
+                        intent = Intent(this, TrialVariantsActivity::class.java)
                     }
                 }
             }
-            else ->{
-                Log.e(TAG,"ERROR!")
+
+            else -> {
+                Log.e(TAG, "ERROR!")
             }
         }
-        intent.putExtra("position",position+1)
+        intent.putExtra("position", position + 1)
         startActivity(intent)
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == android.R.id.home) {
-            onBackPressed()
+            if (userSelectedMode == "selectTrialVariantExercise") {
+                userSelectedMode = "trialVariants"
+                val sharedPref: SharedPreferences =
+                    this.getSharedPreferences("MyPref", Context.MODE_PRIVATE)
+                val editor = sharedPref.edit()
+                editor.putString("userSelectedMode", userSelectedMode)
+                editor.apply()
+                createRecyclerView()
+            } else {
+                intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun checkAnswers() {
+        val sharedPref = applicationContext.getSharedPreferences("MyPref", Context.MODE_PRIVATE)
+        val string = sharedPref.getString("answers", "").toString()
+        Log.d(TAG, string)
+        var userRawAnswers = string.split("/").drop(1)
+        var userAnswers = ArrayList<String>()
+        for (i in 1..29) {
+            userAnswers.add("")
+        }
+        var number: Int
+        var answer: String
+        for (i in userRawAnswers.indices) {
+            number = userRawAnswers[i].split(":")[0].toInt()
+            answer = userRawAnswers[i].split(":")[1]
+            userAnswers[number - 1] = answer
+        }
+        val textResourceId = resources.getIdentifier("pr1_1_answers", "string", packageName)
+        val correctAnswers = resources.getString(textResourceId).split(":")
+        var quantityOfCorrectAnswers = 0
+        for (i in correctAnswers.indices) {
+            if (correctAnswers[i] == userAnswers[i]) {
+                quantityOfCorrectAnswers++
+            }
+        }
+        Log.d(TAG, quantityOfCorrectAnswers.toString())
+        popup(quantityOfCorrectAnswers)
+    }
+
+    private fun clearAnswers() {
+        val sharedPref = applicationContext.getSharedPreferences("MyPref", Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.remove("answers")
+        editor.apply()
+    }
+    private fun popup(count:Int){
+        // Создание объекта PopupWindow
+        val popupView = layoutInflater.inflate(R.layout.popup_layout, null)
+        val popupWindow = PopupWindow(popupView, LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+
+// Настройка параметров всплывающего окна
+        popupWindow.isFocusable = true // Разрешить фокус на всплывающем окне
+        popupWindow.isOutsideTouchable = true // Разрешить закрытие всплывающего окна при касании за его пределами
+
+// Отображение всплывающего окна
+        val parentView: View = findViewById(R.id.recyclerView)
+        popupWindow.showAtLocation(parentView, Gravity.CENTER, 0, 0)
+
+        val textview = popupView.findViewById<TextView>(R.id.popupTextView)
+        val button = popupView.findViewById<Button>(R.id.popupButton)
+        val string = "Правильных ответов: $count"
+        textview.text = string
+        button.setOnClickListener {
+            popupWindow.dismiss()
+        }
+
     }
 }
 
--- app/src/main/java/com/vlad/ege_chemistry/TrialVariantsActivity.kt
+++ app/src/main/java/com/vlad/ege_chemistry/TrialVariantsActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.vlad.ege_chemistry.databinding.ActivityTrialVariantsBinding
 
@@ -18,53 +19,32 @@ class TrialVariantsActivity : AppCompatActivity() {
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        val exercise = intent.getIntExtra("position", 1).toString()
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_trial_variants)
-
-        val exercise = intent.getIntExtra("position",1).toString()
-
-        when (exercise) {
-            "30" -> {
-                checkAnswers()
-            }
-            "31" -> {
-                clearAnswers()
-            }
-            else -> {
-                inflateExercise(exercise)
-            }
-        }
+        inflateExercise(exercise)
 
         binding.testAnswerButton.setOnClickListener {
             val sharedPref = applicationContext.getSharedPreferences("MyPref", Context.MODE_PRIVATE)
-            var answers = sharedPref.getString("answers","")
-            answers+= "#" + exercise + ":" + binding.testEditText.text.toString().trim()
+            var answers = sharedPref.getString("answers", "")
+            answers += "/" + exercise + ":" + binding.testEditText.text.toString().trim()
             val editor = sharedPref.edit()
-            editor.putString("answers",answers)
-            editor.putString("userSelectedMode","trialVariants")
+            editor.putString("answers", answers)
+            editor.putString("userSelectedMode", "trialVariants")
             editor.apply()
-            intent = Intent(this,RecyclerViewActivity::class.java)
-            intent.putExtra("userSelectedMode","selectTrialVariantExercise")
+            Log.d(TAG,answers.toString())
+            intent = Intent(this, RecyclerViewActivity::class.java)
+            intent.putExtra("userSelectedMode", "selectTrialVariantExercise")
             startActivity(intent)
         }
     }
-
-    fun clearAnswers() {
+    private fun inflateExercise(exercise: String) {
         val sharedPref = applicationContext.getSharedPreferences("MyPref", Context.MODE_PRIVATE)
-        val editor = sharedPref.edit()
-        editor.remove("answers")
-        editor.apply()
-    }
-
-    fun checkAnswers() {
-        TODO("Not yet implemented")
-    }
-
-    private fun inflateExercise(exercise:String) {
-        val sharedPref = applicationContext.getSharedPreferences("MyPref", Context.MODE_PRIVATE)
-        val prNumber = sharedPref.getString("prNumber","1")
-        Log.d(TAG,prNumber.toString())
-        Log.d(TAG,exercise.toString())
-        val textResourceId = resources.getIdentifier("pr${prNumber}_$exercise","string",packageName)
+        val prNumber = sharedPref.getString("prNumber", "1")
+        Log.d(TAG, prNumber.toString())
+        Log.d(TAG, exercise.toString())
+        val textResourceId =
+            resources.getIdentifier("pr${prNumber}_$exercise", "string", packageName)
         binding.testTextView.text = resources.getString(textResourceId)
         binding.testEditText.setText("")
     }
@@ -73,9 +53,11 @@ class TrialVariantsActivity : AppCompatActivity() {
         if (item.itemId == android.R.id.home) {
             val sharedPref = applicationContext.getSharedPreferences("MyPref", Context.MODE_PRIVATE)
             val editor = sharedPref.edit()
-            editor.putString("userSelectedMode","trialVariants")
+            editor.putString("userSelectedMode", "selectTrialVariantExercise")
             editor.apply()
-            onBackPressed()
+            intent = Intent(this, RecyclerViewActivity::class.java)
+            intent.putExtra("userSelectedMode", "selectTrialVariantExercise")
+            startActivity(intent)
             return true
         }
         return super.onOptionsItemSelected(item)
